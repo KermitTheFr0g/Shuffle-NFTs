4b9584d3344461423526e53ad8b50694d8e264b8
Kermit2626 <oligray26@gmail.com>
2022-10-11 08:42:43 +0100

    change metadata

--- main.js
+++ main.js
@@ -44,3 +44,25 @@ currentJsons.forEach( nft => {
     i++
     console.log(`Changed: ${nft} - ${shuffledJsons[i]}`);
 })
+
+fs.rmSync('./images');
+fs.rmSync('./jsons');
+fs.renameSync('./shuffledImages', './images');
+fs.renameSync('./shuffledJsons', './jsons');
+
+
+// put vars for description and ipfs to replace in jsons
+const metaData = fs.readdirSync('./jsons');
+
+const CollectionName = 'ENTER COLLECTION NAME';
+const IPFS = 'ENTER IPFS'
+
+metaData.forEach( nft => {
+    let edition = nft.split('.')[0];
+    let file = JSON.parse( fs.readFileSync(`./jsons/${nft}`));
+
+    file.name = `${CollectionName} #${edition}`;
+    file.image = `ipfs://${file.image.split('/')[1]}${IPFS}/${edition}.png`;
+    file.edition = edition;
+    fs.writeFileSync(`./jsons/${nft}`, JSON.stringify(file), null, '/t');
+})
